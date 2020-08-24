36ceddfb04f43313daebd2e8791e3ada377acfc2
AvdheshNigam <kumarnigam28@gmail.com>
2020-08-25 02:30:37 +0530

    Page Layout - mobile menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ const menuIcon = document.querySelector('.js-menu-icon');
 const menuIconClose = document.querySelector('.js-menu-icon span');
 const modalEle = document.querySelector(".modal");
 const modalImage = document.querySelector(".js-modal-image");
-modalClose = document.querySelector(".js-close");
+const modalClose = document.querySelector(".js-close");
 
 menuIcon.addEventListener('click', e => {
   e.preventDefault();
@@ -13,11 +13,11 @@ menuIcon.addEventListener('click', e => {
 
 Array.from(document.querySelectorAll(".js-img-thumb")).forEach(item => {
   item.addEventListener("click", e => {
-    modalEle.style.display = "block";
+    modalEle.classList.add("open");
     modalImage.src = e.target.src;
   });
 });
 
 modalClose.addEventListener("click", () => {
-  modalEle.style.display = "none";
+  modalEle.classList.remove("open");
 });
