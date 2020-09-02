146da3ea7efb211aa084a664b26ead26ab9bb0a1
AvdheshNigam <kumarnigam28@gmail.com>
2020-09-02 13:20:19 +0530

    Random User List

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+const url = 'https://randomuser.me/api/?results=12&gender=male';
 let cardTemplate = '';
 
 function getCard(data) {
@@ -24,14 +25,14 @@ function createCardView(data) {
   mainContainer[0].innerHTML += cardTemplate;
 }
 
-fetch('https://randomuser.me/api/?results=12&gender=male')
-  .then(function (response) {
-    return response.json();
+fetch(url)
+  .then(function (res) {
+    return res.json();
   })
   .then(function (data) {
     console.log(data);
     createCardView(data);
   })
   .catch(function () {
-    console.log("Error something went wrong!");
+    console.log("Error something went wrong! Please try again later. ");
   });   
